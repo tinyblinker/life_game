911a2f44f5caab91135b6ed4a4377403cc11d399
tinyblinker <2149934895@qq.com>
2026-08-15 16:56:08 +0800

    finished all, successfully runned

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ struct MyCell {
     cols: usize,
     rows: usize,
     alive_around_cnt: usize,
+    is_father: bool,
 }
 
 // draw the current cells' status
@@ -17,7 +18,7 @@ fn draw(arr: &Vec<Vec<MyCell>>) -> () {
     for i in arr {
         for j in i {
             match j.is_alive {
-                true => print!("+"),
+                true => print!("O"),
                 false => print!("-"),
             }
         }
@@ -25,25 +26,27 @@ fn draw(arr: &Vec<Vec<MyCell>>) -> () {
     }
 }
 
-// set alive cells
-fn set_alive(arr: &mut Vec<Vec<MyCell>>) -> () {
-    // 2*2
-    arr[10][10].is_alive = true;
-    arr[11][10].is_alive = true;
-    arr[10][11].is_alive = true;
-    arr[11][11].is_alive = true;
+// set alive cells (fathers)
+// fn set_fathers_alive(arr: &mut Vec<Vec<MyCell>>, cols: usize, rows: usize) -> () {
+//     let direction: [(i32, i32); 4] = [(0, 0), (1, 0), (0, 1), (1, 1)];
+//     for (dc, dr) in direction {
+//         let p_cols: usize = (cols as i32 + dc) as usize;
+//         let p_rows: usize = (rows as i32 + dr) as usize;
+//         arr[p_cols][p_rows].is_alive = true;
+//         arr[p_cols][p_rows].is_father = true;
+//     }
+// }
 
-    // 2*2
-    arr[16][41].is_alive = true;
-    arr[16][40].is_alive = true;
-    arr[15][41].is_alive = true;
-    arr[15][40].is_alive = true;
+// set alive cells (seeds)
+fn set_seeds_alive(arr: &mut Vec<Vec<MyCell>>, cols: usize, rows: usize) -> () {
+    // other cells to be alive
+    arr[cols][rows].is_alive = true;
 }
 
 // count_cells_around
 fn count_cells_around(arr: &Vec<Vec<MyCell>>, cols: usize, rows: usize) -> i32 {
-    let max_cols = arr.len();
-    let max_rows = arr[0].len();
+    let max_cols = arr.len() - 1;
+    let max_rows = arr[0].len() - 1;
 
     // exceptions (do not include borders)
     if max_cols == cols || 0 == cols {
@@ -76,10 +79,10 @@ fn count_cells_around(arr: &Vec<Vec<MyCell>>, cols: usize, rows: usize) -> i32 {
 }
 
 fn update_alive_around_count(arr: &mut Vec<Vec<MyCell>>) -> () {
-    let max_cols: usize = arr.len();
-    let max_rows: usize = arr[0].len();
-    for i in (0..max_cols) {
-        for j in (0..max_rows) {
+    let max_cols: usize = arr.len() - 1;
+    let max_rows: usize = arr[0].len() - 1;
+    for i in 0..max_cols {
+        for j in 0..max_rows {
             match count_cells_around(&arr, i, j) {
                 -1 => continue,
                 count => arr[i][j].alive_around_cnt = count as usize,
@@ -89,10 +92,13 @@ fn update_alive_around_count(arr: &mut Vec<Vec<MyCell>>) -> () {
 }
 
 fn update_mycells(arr: &mut Vec<Vec<MyCell>>) -> () {
-    let max_cols: usize = arr.len();
-    let max_rows: usize = arr[0].len();
+    let max_cols: usize = arr.len() - 1;
+    let max_rows: usize = arr[0].len() - 1;
     for i in 0..max_cols {
         for j in 0..max_rows {
+            if arr[i][j].is_father {
+                continue;
+            }
             match arr[i][j].is_alive {
                 true => {
                     if arr[i][j].alive_around_cnt < 2 {
@@ -112,14 +118,59 @@ fn update_mycells(arr: &mut Vec<Vec<MyCell>>) -> () {
 }
 
 fn flush_the_screen() -> () {
-    print!("\x1b[2J\x1b[3J\x1b[H");
+    print!("\x1B[2J\x1B[H");
     io::stdout().flush().unwrap();
+    // thread::sleep(Duration::from_secs_f32(0.1)); // test ok!
+}
+
+fn draw_the_gun(arr: &mut Vec<Vec<MyCell>>) -> () {
+    let directions: [(usize, usize); 36] = [
+        (24, 0),
+        (22, 1),
+        (24, 1),
+        (12, 2),
+        (13, 2),
+        (20, 2),
+        (21, 2),
+        (34, 2),
+        (35, 2),
+        (11, 3),
+        (15, 3),
+        (20, 3),
+        (21, 3),
+        (34, 3),
+        (35, 3),
+        (0, 4),
+        (1, 4),
+        (10, 4),
+        (16, 4),
+        (20, 4),
+        (21, 4),
+        (0, 5),
+        (1, 5),
+        (10, 5),
+        (14, 5),
+        (16, 5),
+        (17, 5),
+        (22, 5),
+        (24, 5),
+        (10, 6),
+        (16, 6),
+        (24, 6),
+        (11, 7),
+        (15, 7),
+        (12, 8),
+        (13, 8),
+    ];
+    for (dr, dc) in directions {
+        set_seeds_alive(arr, dr + 2, dc + 25);
+    }
 }
 
 fn main() {
     // init an array
-    let cols: usize = 50;
-    let rows: usize = 50;
+    let cols: usize = 40;
+    let rows: usize = 100;
     let mut arr: Vec<Vec<MyCell>> = (0..cols)
         .map(|p_cols| {
             (0..rows)
@@ -128,17 +179,29 @@ fn main() {
                     cols: p_cols,
                     rows: p_rows,
                     alive_around_cnt: 0,
+                    is_father: false,
                 })
                 .collect()
         })
         .collect();
     // functions to process
-    set_alive(&mut arr);
-    //    loop {
-    thread::sleep(Duration::from_secs(3));
-    //        update_alive_around_count(&mut arr);
-    //        update_mycells(&mut arr);
-    draw(&arr);
-    flush_the_screen();
-    //    }
+    // set_fathers_alive(&mut arr, 10, 10); // test ok!
+    // set_fathers_alive(&mut arr, 25, 40); // test ok!
+    // set_seeds_alive(&mut arr, 12, 12);
+    // set_seeds_alive(&mut arr, 12, 14);
+    // set_seeds_alive(&mut arr, 13, 15);
+    // set_seeds_alive(&mut arr, 14, 15);
+    // set_seeds_alive(&mut arr, 15, 12);
+    // set_seeds_alive(&mut arr, 15, 15);
+    // set_seeds_alive(&mut arr, 16, 13);
+    // set_seeds_alive(&mut arr, 16, 14);
+    // set_seeds_alive(&mut arr, 16, 15);
+    draw_the_gun(&mut arr); // draw the gun
+    loop {
+        draw(&arr); // test ok!
+        thread::sleep(Duration::from_secs_f32(0.15)); // test ok!
+        flush_the_screen(); // test ok!
+        update_alive_around_count(&mut arr);
+        update_mycells(&mut arr);
+    }
 }
